5f594af70467a6fe115eac4903c30b1c8c48299e
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-07 16:29:00 +0300

    - Using React Custom Hooks instead of useState for controlled form elements (useInput)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 
+//Custom react hook with use as prefix
+function useInput(initialValue){
+  const [value, setValue] = useState(initialValue);
+  //Return two properties
+  return [
+    {
+      value, 
+      onChange: (e) => setValue(e.target.value)
+    },
+    () => setValue(initialValue)
+  ];
+}
 
-function App({}) {
-  const [title, setTitle] = useState("");
-  const [color , setColor] = useState("#00000");
+function App() {
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps , resetColor] = useState("#00000");
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    setTitle("");
-    setColor("#00000");
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle();
+    resetColor("#00000");
   };
 
   return (
     <form onSubmit={submit}>
       <input type="text" 
-        onChange={event => setTitle(event.target.value)} 
-        value={title} 
+        {...titleProps}
         placeholder='color titles'
       />
-      <input  
-        onChange={ event => setColor(event.target.value)}
-        type="color" 
-        value={color}/>
+      <input {...colorProps} type="color" 
+       />
       <button>ADD</button>
     </form>
   );
