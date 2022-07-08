88bda7222c52f1c09f92041df994ef70de1a4d63
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-08 11:28:50 +0300

    - Using Fetch On a GraphQL API

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-function GithubUser({name, location, avatar}){
+// Using GraphQL
+
+function Lift({name, elevationGain, status}){
   return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img height={150} src={avatar} alt={name}/>
+      <p>{elevationGain} {status}</p>
     </div>
   )
 }
@@ -19,11 +20,29 @@ function App() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  //GraphQL
+  const query = `
+  query 
+  {
+    allLifts{
+      name
+      elevationGain
+      status
+    }
+  }
+  `;
+
+  const opts = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({query})
+  }
+
   useEffect(() => {
 
     setLoading(true);
 
-    fetch(`https://api.github.com/users/michaelmburu`)
+    fetch(`https://snowtooth.moonhighway.com`, opts)
     .then(response => response.json())
     .then(setData) //same as data => SetData(data)
     .then(() => setLoading(false))
@@ -34,7 +53,12 @@ function App() {
     if(error) return <pre>{JSON.stringify(error)}</pre>
     if(!data) return null;
     return (
-      <GithubUser name={data.name} location ={data.location} avatar = {data.avatar_url} />
+      <div>
+         {data.data.allLifts.map((lift) => (
+            <Lift name={lift.name} elevationGain = {lift.elevationGain} status={lift.status} />
+         ))}
+      </div>
+   
     );
 }
 
