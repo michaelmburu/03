d8a82aff52a908f66d233f7ce203a323712bb43c
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-08 14:39:23 +0300

    - Render Prps

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,64 +1,32 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// Using GraphQL
+//Working wih render props
 
-function Lift({name, elevationGain, status}){
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{elevationGain} {status}</p>
-    </div>
+const mtkenya_peaks = [
+  {name: "Lenana", elevation: 4800},
+  {name: "Batian", elevation: 5199}, 
+  {name: "Nelion", elevation: 4985}
+]
+
+function List({data, renderItem, renderEmpty}){
+  return !data.length ? (renderEmpty) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   )
 }
-//Using API's in React
 
 function App() {
 
-  const [data, setData] = useState(null);
-  //Handling loading states
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  //GraphQL
-  const query = `
-  query 
-  {
-    allLifts{
-      name
-      elevationGain
-      status
-    }
-  }
-  `;
-
-  const opts = {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({query})
-  }
-
-  useEffect(() => {
-
-    setLoading(true);
-
-    fetch(`https://snowtooth.moonhighway.com`, opts)
-    .then(response => response.json())
-    .then(setData) //same as data => SetData(data)
-    .then(() => setLoading(false))
-    .catch(setError)
-  }, []);
-
-    if(loading) return <h1>Loading...</h1>;
-    if(error) return <pre>{JSON.stringify(error)}</pre>
-    if(!data) return null;
     return (
-      <div>
-         {data.data.allLifts.map((lift) => (
-            <Lift name={lift.name} elevationGain = {lift.elevationGain} status={lift.status} />
-         ))}
-      </div>
-   
+     <List 
+        data={mtkenya_peaks} 
+        renderEmpty={<p>This list is empty</p>}
+        renderItem={item => <>{item.name} {item.elevation}</>} 
+     />
     );
 }
 
