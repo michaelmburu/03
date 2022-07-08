df315841e1acdd3b6f85b7cffb14c6ddd91bae4f
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-08 11:07:53 +0300

    - Handling Error States

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,13 +15,24 @@ function GithubUser({name, location, avatar}){
 function App() {
 
   const [data, setData] = useState(null);
+  //Handling loading states
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+
+    setLoading(true);
+
     fetch(`https://api.github.com/users/michaelmburu`)
     .then(response => response.json())
     .then(setData) //same as data => SetData(data)
+    .then(() => setLoading(false))
+    .catch(setError)
   }, []);
 
-  if(data)
+    if(loading) return <h1>Loading...</h1>;
+    if(error) return <pre>{JSON.stringify(error)}</pre>
+    if(!data) return null;
     return (
       <GithubUser name={data.name} location ={data.location} avatar = {data.avatar_url} />
     );
